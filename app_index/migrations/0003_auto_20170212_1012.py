27c87f47864b575033c44cc83347a681f874a1fb
bogdan-macbook <bogdanned32@gmail.com>
2017-02-12 18:49:18 +0100

    asd

--- app_index/migrations/0003_auto_20170212_1012.py
+++ app_index/migrations/0003_auto_20170212_1012.py
@@ -3,10 +3,3 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('app_index', '0002_auto_20170212_1011'),
-    ]
